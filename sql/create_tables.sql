423f43f680f537d8a6d502aca7fb4c1547449952
lavasudevan <v_ln@yahoo.com>
2019-06-16 20:06:16 -0700

    added dropped catches table

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -11,6 +11,18 @@ create table if not exists game (
   innings2_id integer 
 );
 
+alter table game
+add wonby text;
+alter table game 
+add team1_score integer;
+alter table game 
+add team1_overs real;
+alter table game 
+add team2_score integer;
+alter table game 
+add team2_overs real;
+
+
 create table if not exists 
 innings_index (
   type text primary key,
@@ -40,3 +52,11 @@ bowl_innings (
   primary key(id, player_id)
    
 );
+
+
+create table if not exists
+dropped_catches(
+  innings_id int,
+  player_id integer
+);
+
